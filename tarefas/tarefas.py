44be208c1d57439a099a3f2b32d2629dd6176e42
rhuan.gonzaga <rhuangonzaga22@gmail.com>
2024-12-09 11:20:26 -0300

    Criando rota para atualizar o status

--- tarefas/tarefas.py
+++ tarefas/tarefas.py
@@ -4,7 +4,7 @@ from database import db
 
 
 tarefas_blueprint = Blueprint('tarefa',__name__,template_folder='templates')
-
+#Criando uma nova tarefa
 @tarefas_blueprint.route('/tarefa', methods=['POST'])
 def criar_tarefa():
   data = request.json
@@ -13,3 +13,20 @@ def criar_tarefa():
   db.session.add(nova_tarefa)
   db.session.commit()
   return jsonify({"mensagem":"Tarefa cadastrada"}), 201
+
+#Editando o status de uma tarefa
+@tarefas_blueprint.route('/status/<int:id>',methods=['POST'])
+def mudar_status(id):
+  data = request.json
+
+  tarefa = Tarefas.query.id(id)
+
+  if not tarefa:
+    return jsonify({"erro": "Tarefa não encontrada"}), 404
+  
+  tarefa.status = data.get('status', tarefa.status)
+
+  db.session.commit()
+
+  return jsonify({"Mensagem": "Tarefa atualizada com sucesso"}), 201
+

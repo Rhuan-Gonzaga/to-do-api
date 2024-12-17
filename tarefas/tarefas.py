4e17bce20a1b86ca0c83c9a2ae0008ee13271289
rhuan-gonzaga <rhuangonzaga22@gmail.com>
2024-12-16 21:11:48 -0300

    Mudando o status da tatefa e deletando a tarefa

--- tarefas/tarefas.py
+++ tarefas/tarefas.py
@@ -2,8 +2,8 @@ from flask import Blueprint,request,jsonify
 from tarefas.models import Tarefas
 from database import db
 
-
 tarefas_blueprint = Blueprint('tarefa',__name__,template_folder='templates')
+
 #Criando uma nova tarefa
 @tarefas_blueprint.route('/tarefa', methods=['POST'])
 def criar_tarefa():
@@ -15,18 +15,31 @@ def criar_tarefa():
   return jsonify({"mensagem":"Tarefa cadastrada"}), 201
 
 #Editando o status de uma tarefa
-@tarefas_blueprint.route('/status/<int: id>',methods=['POST'])
+@tarefas_blueprint.route('/status/<int:id>',methods=['POST'])
 def mudar_status(id):
   data = request.json
 
-  tarefa = Tarefas.query.id(id)
-
+  #verifiando se a tarefa existe
+  tarefa = Tarefas.query.get(id)
   if not tarefa:
     return jsonify({"erro": "Tarefa não encontrada"}), 404
   
+  #Pegando o novo statur da tarefa e atualizando no banco
   tarefa.status = data.get('status', tarefa.status)
-
   db.session.commit()
 
   return jsonify({"Mensagem": "Tarefa atualizada com sucesso"}), 201
 
+#Deletando uma tarefa pelo ID
+@tarefas_blueprint.route('/deletartarefa/<int:id>',methods=['DELETE'])
+def deletar_tarefa(id):
+
+  #verifiando se a tarefa existe
+  tarefa = Tarefas.query.get(id)
+  if not tarefa:
+    return jsonify({"erro": "Tarefa não encontrada"}), 404
+
+  Tarefas.query.filter_by(id=Tarefas.tarefa).delete()
+  db.session.commit() 
+  
+  return jsonify({"Mensagem": "Tarefa deletada com sucesso"}), 201

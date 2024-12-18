bc2d7dcc7c992fd519e0fdf60e97a69075f05645
rhuan.gonzaga <rhuangonzaga22@gmail.com>
2024-12-18 14:12:06 -0300

    Colocando um hash na senha de usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,9 +1,15 @@
 from database import db
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    usuario = db.Column(db.String(100), nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
-   
+    usuario = db.Column(db.String(100), nullable=False, unique=True)  
+    senha = db.Column(db.String(120), nullable=False)
     
+    # Método para definir o hash da senha
+    def set_password(self, password):
+        self.senha = generate_password_hash(password)  
+
+    # Método para verificar a senha
+    def check_password(self, password):
+        return check_password_hash(self.senha, password)
--- usuarios/usuarios.py
+++ usuarios/usuarios.py
@@ -7,7 +7,21 @@ usuarios_blueprint= Blueprint('usuario',__name__,template_folder='templates')
 @usuarios_blueprint.route('/criarusuario', methods=['POST'])
 def criar_usuario():
     data = request.json
-    novo_usuario = Usuario(usuario=data['usuario'], senha=data['senha'])
-    db.session.add(novo_usuario)
+    usarioo = data.get("usuario")
+    senha = data.get("senha")
+
+    if not usarioo or not senha:
+        return jsonify({"erro": "Usuário e senha são obrigatórios"}), 400
+    
+    # Verificar se o usuário já existe
+    if Usuario.query.filter_by(usuario=data['usuario']).first():
+        return jsonify({"erro": "Usuário já existe"}), 409
+
+    # Criar um novo usuário
+    novo_usario = Usuario(usuario=usarioo)
+    print(senha)
+    novo_usario.set_password(senha)
+    db.session.add(novo_usario)
     db.session.commit()
+
     return jsonify({"mensagem": "Usuario cadastrado"}), 201
